48790e04f17604e6f825d5ea7d72272ae1ccb27e
Jessen <jewinardinata@csumb.edu>
2018-04-09 22:19:00 -0700

    initial service
    hello csumb

--- src/main/java/com/example/microservicesbasics/DemoApplication.java
+++ src/main/java/com/example/microservicesbasics/DemoApplication.java
@@ -2,10 +2,21 @@ package com.example.microservicesbasics;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class DemoApplication {
 
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello CSUMB!";
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
